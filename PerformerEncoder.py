cbb82d44bf0c0d595b2c1032f8ba54513a8d6f7e
Junyuan Qiu <129668587+James-JY-Qiu@users.noreply.github.com>
2024-04-22 11:46:27 -0400

    create load_data and Performer encoder

--- PerformerEncoder.py
+++ PerformerEncoder.py
@@ -1,91 +1,115 @@
-import numpy as np
 import torch
 from PerformerModel.Performer import Performer
+from utils import load_data
+from torch.utils.data import DataLoader
 
 
 class PerformerEncoder:
     def __init__(
             self,
             dataset_name,
-            images,
-            position_encoding_dim=128,
+            patch_size=(4, 4),
+            linear_embedding_dim=256,
             performer_params=None,
             dtype=torch.float32
     ):
+        # ================ Data ================
         assert dataset_name in ['mnist', 'cifar'], 'Dataset must be either "mnist" or "cifar"'
         self.dataset_name = dataset_name
-        self.images = images
-        self.position_encoding_dim = position_encoding_dim
-        self.performer_params = performer_params.copy()
+        # ================ Parameters ================
+        self.patch_height, self.patch_width = patch_size
+        self.performer_params = performer_params if performer_params is not None else {
+            'dim': linear_embedding_dim,
+            'depth': 12,
+            'heads': 8,
+            'dim_head': 64
+        }
         self.dtype = dtype
-
-        if dataset_name == 'mnist':
-            self.input_shape = (1, 28, 28)
-            self.performer_params['dim'] = 28 * 28
-        elif dataset_name == 'cifar':
-            self.input_shape = (3, 32, 32)
-            self.performer_params['dim'] = 32 * 32 * 3
-        else:
-            raise ValueError('Invalid dataset name')
-
-        position_enc = self.generate_positional_encoding()
-        self.position_enc_tensor = torch.tensor(position_enc, dtype=self.dtype)
+        # ================ Model ================
+        # Initialize the performer model
         self.performer = Performer(**self.performer_params)
+        # Initialize the linear layer
+        linear_in_features = patch_size[0] * patch_size[1] * (3 if dataset_name == 'cifar' else 1)
+        self.linear_embedding = torch.nn.Linear(in_features=linear_in_features, out_features=linear_embedding_dim)
+        # Positional encoding
+        num_patches = self._check_patch_size()
+        self.positional_embedding = torch.nn.Parameter(torch.zeros(num_patches, linear_embedding_dim))
+        torch.nn.init.uniform_(self.positional_embedding, -0.01, 0.01)
 
-    def flatten_images(self):
+    def _check_patch_size(self):
+        if self.dataset_name == 'mnist':
+            height = width = 28
+        elif self.dataset_name == 'cifar':
+            height = width = 32
+        else:
+            raise ValueError(f'Unknown dataset: {self.dataset_name}')
+
+        assert height % self.patch_height == 0 and width % self.patch_width == 0, "Image size must be divisible by patch size"
+
+        return (height // self.patch_height) * (width // self.patch_width)
+
+    def _get_patches(self, image, channels, num_patches):
+        """Unfold the image into patches
+
+        :param image: one image
+        :param channels: number of channels
+        :param num_patches: number of patches
+        :return: patches
         """
-        Flatten images from the data loader.
+        patches = image.unfold(1, self.patch_height, self.patch_height).unfold(2, self.patch_width, self.patch_width)
+        patches = patches.contiguous().view(channels, num_patches, self.patch_height * self.patch_width)
+        patches = patches.permute(1, 0, 2).reshape(num_patches, -1)
+        return patches
 
-        Returns:
-        - Flattened images of shape (N, C*H*W).
+    def _linear_embedding(self, batch):
+        """Linearly embed the patches
+
+        :param batch: batch of images
+        :return: embeddings
         """
-        return self.images.view(self.images.size(0), -1)
+        batch_size, channels, height, width = batch.size()
+        num_patches_height = height // self.patch_height
+        num_patches_width = width // self.patch_width
+        num_patches = num_patches_height * num_patches_width
 
-    def generate_positional_encoding(self):
+        embeddings = []
+        for img in batch:
+            # Unfold the image into patches
+            patches = self._get_patches(img, channels, num_patches)
+            # Linearly embed the patches
+            embedded_patches = self.linear_embedding(patches)
+            embeddings.append(embedded_patches)
+
+        embeddings = torch.stack(embeddings)
+
+        return embeddings
+
+    def encode(self, batch):
+        """Encode the batch of images
+
+        :param batch: batch of images
+        :return: performer encoding
         """
-        Generate positional encoding for an image.
+        # Linearly embed the patches
+        embeddings = self._linear_embedding(batch)
+        # Add positional encoding
+        embeddings += self.positional_embedding
+        # Performer encoding
+        encoding = self.performer(embeddings)
+        return encoding
 
-        Returns:
-        - Positional encoding for the image of shape (C*H*W, position_encoding_dim).
-        """
-        # Generate a grid of coordinates
-        C, H, W = self.input_shape
-        y, x, c = np.meshgrid(np.arange(H), np.arange(W), np.arange(C), indexing='ij')
-        y = y.flatten()
-        x = x.flatten()
-        c = c.flatten()
 
-        # Compute the positional encodings
-        position_enc = np.zeros((H * W * C, self.position_encoding_dim))
-        for i in range(self.position_encoding_dim // 3):
-            div_term = np.power(10000, (3 * i) / self.position_encoding_dim)
-            position_enc[:, 3 * i] = np.sin(y / div_term)
-            position_enc[:, 3 * i + 1] = np.cos(x / div_term)
-            position_enc[:, 3 * i + 2] = np.sin(c / div_term)
+if __name__ == '__main__':
+    data = load_data()
+    data_loader = DataLoader(data['cifar'][0], batch_size=32, shuffle=True)
+    encoder = PerformerEncoder(dataset_name='cifar')
+    for batch, _ in data_loader:
+        embeddings = encoder.encode(batch)
+        print(embeddings.size())
+        break
 
-        return position_enc
 
-    def preprocessing(self):
-        """
-        Preprocess the data.
 
-        Returns:
-        - Preprocessed data.
-        """
-        flattened_images = self.flatten_images()
-        combine_feature = flattened_images.unsqueeze(-1) * self.position_enc_tensor.unsqueeze(0)
-
-        return combine_feature
-
-    def encoding(self):
-        """
-        Encode the data.
-
-        Returns:
-        - Encoded data.
-        """
-        combine_feature = self.preprocessing()
-        return self.performer(combine_feature)
 
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torchvision
 import torchvision.transforms as transforms
 
